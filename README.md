24ae45264721a5a3a704894d8ba6ba6ea464ffdd
seoco <seokho.dev@gmail.com>
2021-03-15 22:09:38 +0900

    chore: prepare project

---
+++ README.md
@@ -0,0 +1,5 @@
+# type-csv-transformer
+
+This library is still in development.
+
+[Initial concept](https://gist.github.com/seocochan/746b2d8f136f012354f4ad3f403db5c8)
---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+    },
+  },
+};
+export default config;
